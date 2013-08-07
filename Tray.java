0599e71eaded07812f6e954f55f8b407023ba18d
winstonjhuang <winstonjhuang@gmail.com>
2013-08-06 21:53:05 -0700

    getmoney getpaid

--- Tray.java
+++ Tray.java
@@ -6,62 +6,45 @@ public class Tray {
   private ArrayList<Block> blocks;
 	
 	public Tray() {
+
+		blocks = new ArrayList<Block>();
 		
 	}
 	
-	/* Returns true if the current Block can be moved to Point p, and false otherwise
-	 */
-	public boolean validMove (Block currentBlock, Point p) {
-		if (direction(currentBlock.getTopLeft(), p).equals("up")) {
-			for (Block block: blocks) {
-				if ((block.getTopLeft().x - block.height()) == currentBlock.getTopLeft().x) {
-					return false;
-				}
-			}
+	//Add a block to the tray.
+	public void addBlock(Block b) {
+
+		if (validMove(b, b.trow(), b.lcol())) {
+			blocks.add(b);
 		}
-		if (direction(currentBlock.getTopLeft(), p).equals("down")) {
-			for (Block block: blocks) {
-				if (block.getTopLeft().x == (currentBlock.getTopLeft().x - currentBlock.height())) {
-					return false;
-				}
-			}
+	}
+
+	//Move a block.
+	public void moveBlock(Block b, int trowDest, lcolDest) {
+
+		if (validMove(b, trowDest, lcolDest)) {
+
 		}
-		if (direction(currentBlock.getTopLeft(), p).equals("right")) {
-			for (Block block: blocks) {
-				if (block.getTopLeft().y == (currentBlock.getTopLeft().y + currentBlock.width())) {
-					return false;
-				}
+	} 
+
+	//Returns true if the current Block can be moved to Point p, and false otherwise
+	public boolean validMove (Block b, int trowDest, int lcolDest) {
+
+
+		int browDest = trowDest + b.height();
+		int rcolDest = lcolDest + b.width();
+
+		for (Block block : blocks) {
+
+			if (trowDest < block.brow() && browDest > block.trow()) {
+				if (rcolDest >= block.lcol() && lcolDest <= block.rcol()) {
+						return false;
+					}
 			}
+
 		}
-		if (direction(currentBlock.getTopLeft(), p).equals("left")) {
-			for (Block block: blocks) {
-				if ((block.getTopLeft().y + block.width()) == currentBlock.getTopLeft().y) {
-					return false;
-				}
-			}
-		}
+
 		return true;
 	}
 
-	/* Returns the direction when moving from p1 to p2.
-	 */
-	public String direction (Point p1, Point p2) {
-		if (p1.x == p2.x) {
-			if (p1.y > p2.y) {
-				return "up";
-			}
-			if (p1.y < p2.y) {
-				return "down";
-			}
-		}
-		if (p1.y == p2.y) {
-			if (p1.x > p2.x) {
-				return "right";
-			}
-			if (p1.y < p2.y) {
-				return "left";
-			}
-		}
-		return "diagonal";
-	}
 }
