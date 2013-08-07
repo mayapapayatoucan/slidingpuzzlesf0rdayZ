846a6132168efa161d57066e9d466c7d0fe26f79
bslaughter <bradslaughter@berkeley.edu>
2013-08-06 23:22:00 -0700

    Update Tray.java

--- Tray.java
+++ Tray.java
@@ -2,49 +2,56 @@ import java.awt.*;
 import java.util.*;
 
 public class Tray {
-
-  private ArrayList<Block> blocks;
 	
-	public Tray() {
+	private boolean debug = true;
+	
 
-		blocks = new ArrayList<Block>();
-		
+	private ArrayList<Block> blocks = new ArrayList<Block>();
+	private int trayWidth;
+	private int trayHeight;
+	
+	public Tray(int width, int height) {
+		trayWidth = width;
+		trayHeight = height;
 	}
 	
-	//Add a block to the tray.
-	public void addBlock(Block b) {
-
-		if (validMove(b, b.trow(), b.lcol())) {
+	public void addBlock (Block b) {
+		if (b != null){
 			blocks.add(b);
 		}
 	}
-
-	//Move a block.
-	public void moveBlock(Block b, int trowDest, lcolDest) {
-
-		if (validMove(b, trowDest, lcolDest)) {
-
-		}
-	} 
-
-	//Returns true if the current Block can be moved to Point p, and false otherwise
+	
 	public boolean validMove (Block b, int trowDest, int lcolDest) {
 
-
+		if (!blocks.contains(b)){
+			return false;   // can only move blocks on the board
+		}
+		if ((trowDest < 0) || (trowDest > trayHeight) || (lcolDest < 0) || (lcolDest > trayWidth)) {
+			return false;   // cannot move off the board
+		}
+		
+		if ((b.trow() != trowDest) && (b.lcol() != lcolDest)) {
+			return false; //cannot move diagonally
+		}
+		
 		int browDest = trowDest + b.height();
 		int rcolDest = lcolDest + b.width();
 
 		for (Block block : blocks) {
-
-			if (trowDest < block.brow() && browDest > block.trow()) {
-				if (rcolDest >= block.lcol() && lcolDest <= block.rcol()) {
-						return false;
-					}
+			if (!b.equals(block)) {
+				if (trowDest <= block.brow() && browDest >= block.trow()) {
+					if (rcolDest >= block.lcol() && lcolDest <= block.rcol()) {
+						
+							//DEBUG
+							if (debug) {
+								System.out.println("Cockblocked by: " + block.trow() + ", " + block.lcol());
+							}
+							return false;
+						}
+				}
 			}
-
 		}
 
 		return true;
 	}
-
 }
