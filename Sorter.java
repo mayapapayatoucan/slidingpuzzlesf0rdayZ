05e043720f55b8970150b5dcd47cbda9876d8515
bslaughter <bradslaughter@berkeley.edu>
2013-08-08 16:40:10 -0700

    Update Sorter.java

--- Sorter.java
+++ Sorter.java
@@ -1,8 +1,10 @@
+import java.util.*;
 
 public class Sorter {
-  
+	
 	private InputSource input;
-	private static Tray tray;
+	private Tray tray;
+	public boolean debug;
 		
 	public Sorter () {
 		// not implemented yet
@@ -12,41 +14,74 @@ public class Sorter {
 		// Based on stuff from InputSource class -- is there an easier way to do this?
 		input = new InputSource(fileName);
 		String s = input.readLine ( );
-		if (fileName.substring(fileName.length() - 4, fileName.length()).equals("goal")) {	// this contains one block, not a whole tray
+		makeTray(s);   // first line sets up the tray dimensions
+		while (true) {
+			s = input.readLine ( );   // making blocks
+			if (s == null) {
+				break;
+			}
 			Parse(s);
-		} else {
-			makeTray(s);   // first line sets up the tray dimensions
-			while (true) {
-				s = input.readLine ( );   // making blocks
-				if (s == null) {
-					break;
-				}
-				Parse(s);
+		}
+	}
+	
+	public Sorter (String fileName, String goalName) {
+		// Based on stuff from InputSource class -- is there an easier way to do this?
+		input = new InputSource(fileName);
+		String s = input.readLine ( );
+		makeTray(s);   // first line sets up the tray dimensions
+		while (true) {
+			s = input.readLine ( );   // making blocks
+			if (s == null) {
+				break;
+			}
+			Parse(s);
+		}
+		input = new InputSource(goalName);
+		while (true) {
+			s = input.readLine ( );   // making blocks
+			if (s == null) {
+				break;
+			}
+			if (isBlock(s)) {
+				tray.addGoalBlock(new Block(s));
 			}
 		}
 	}
 	
-	public static void makeTray (String s) {
+	public void makeTray (String s) {
 		tray = new Tray(Integer.valueOf(s.substring(0, 1)), Integer.valueOf(s.substring(2, 3)));
 	}
 	
-	public static void Parse (String line) {
+	public void Parse (String line) {
 		if (isBlock(line)) {
 			tray.addBlock(new Block(line));
-		} else if (line.equals("-ooptions")) {
-			System.out.println();
 		} else if (line.substring(0, 2).equals("-o")) {
-			debug(line.substring(2, line.length()));
+			String debugCommand = line.substring(2, line.length());
+			if ("options".equals(debugCommand)) {
+				System.out.println("Debugging options:");
+				System.out.println("-oblock: " + "prints debugging output for blocks");
+				System.out.println("-otray: " + "prints debugging output for trays");
+				System.out.println("-osorter: " + "prints debugging output for the sorter");
+			}
+			if ("block".equals(debugCommand)) {
+				for (Block block : tray.getBlocks()) {
+					block.debug = true;
+				}
+			}
+			if ("tray".equals(debugCommand)) {
+				tray.debug = true;
+			}
+			if ("solver".equals(debugCommand)) {
+				debug = true;
+			}
+			System.out.println("Not a valid debugging option. Type -ooptions to see all debugging options.");
+			System.exit(1);
 		} else {
 			
 		}
 	}
 	
-	public static void debug (String s) {
-		// Put debugging options here -- what should we include?
-	}
-	
-	public static boolean isBlock (String line) {
+	public boolean isBlock (String line) {
 		String[] vals = line.split(" ");
 		if (vals.length != 4) {
 			return false;
@@ -63,6 +98,10 @@ public class Sorter {
 	
 	public Tray getTray() {
 		return tray;
+	}
+	
+	public void solve () {
+		
 	}
 	
 	public static void main(String[] args) {
