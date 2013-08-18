89383703c8c11f86a3510bb2792a3faba2d34b69
winstonjhuang <winstonjhuang@gmail.com>
2013-08-17 20:43:51 -0700

    Update Solver.java

--- Solver.java
+++ Solver.java
@@ -1,39 +1,44 @@
 import java.util.*;
 
+/* AUTHORS:
+Zach Gottesman
+Brad Slaughter
+Maya Kulkarni
+Winston Huang
+
+*/
+
 public class Solver {
 
-	private InputSource input;
 	private Tray initTray;
 	public boolean debug;
+	public boolean debugStack;
 
-
-	public Solver (String fileName) {
-		input = new InputSource(fileName);
-		String s = input.readLine ( );
-		makeTray(s);   // first line sets up the tray dimensions
-		while (true) {
-			s = input.readLine ( );   // making blocks
-			if (s == null) {
-				break;
-			}
-			Parse(s);
-		}
-	}
-
+	//create a new solver. Solver takes two string arguments: an initial configuration file name,
+	//and a goal configuration file name.
 	public Solver (String fileName, String goalName) {
-		input = new InputSource(fileName);
-		String s = input.readLine ( );
+
+		//Read initial configuration file.
+		InputSource initFile = new InputSource(fileName);
+		String s = initFile.readLine();
+
 		makeTray(s);   // first line sets up the tray dimensions
 		while (true) {
-			s = input.readLine ( );   // making blocks
+
+			s = initFile.readLine();   // making blocks
+
 			if (s == null) {
 				break;
 			}
 			Parse(s);
 		}
-		input = new InputSource(goalName);
+		
+		//Read goal configuration file.
+		InputSource goalFile = new InputSource(goalName);
 		while (true) {
-			s = input.readLine ( );   // making blocks
+
+			s = goalFile.readLine();   // making blocks
+
 			if (s == null) {
 				break;
 			}
@@ -45,34 +50,53 @@ public class Solver {
 
 	public void makeTray (String s) {
 		String[] dimensions = s.split(" ");
-		initTray = new Tray(Integer.valueOf(dimensions[0]), Integer.valueOf(dimensions[1]));
+
+		try {
+
+			int rows = Integer.parseInt(dimensions[0]);
+			int cols = Integer.parseInt(dimensions[1]);
+
+			initTray = new Tray(rows, cols);
+
+		}
+
+		catch (NumberFormatException e) {}
+
 	}
 
 	public void Parse (String line) {
+
 		if (isBlock(line)) {
 			initTray.addBlock(new Block(line));
-		} else if (line.substring(0, 2).equals("-o")) {
+		} 
+
+		else if (line.substring(0, 2).equals("-o")) {
+			
 			String debugCommand = line.substring(2, line.length());
+			
 			if ("options".equals(debugCommand)) {
 				System.out.println("Debugging options:");
 				System.out.println("-oblock: " + "prints debugging output for blocks");
 				System.out.println("-otray: " + "prints debugging output for trays");
 				System.out.println("-osorter: " + "prints debugging output for the sorter");
 			}
+
 			if ("block".equals(debugCommand)) {
 				for (Block block : initTray.getBlocks()) {
 					block.debug = true;
 				}
 			}
+
 			if ("tray".equals(debugCommand)) {
 				initTray.debug = true;
 			}
+
 			if ("solver".equals(debugCommand)) {
 				debug = true;
 			}
+
 			System.out.println("Not a valid debugging option. Type -ooptions to see all debugging options.");
 			System.exit(1);
-		} else {
 
 		}
 	}
@@ -110,6 +134,11 @@ public class Solver {
 
 		while (!process.isEmpty()) {
 
+			//DEBUG print stack size of trays to process on each iteration
+			if (debugStack) {
+				System.out.println("Solving...stack size: " + process.size());
+			}
+
 			Tray curr = process.removeFirst();
 			ArrayList<Tray> moves = curr.posMoves();
 
@@ -137,8 +166,53 @@ public class Solver {
 
 	public static void main(String[] args) {
 
-		Solver run = new Solver(args[0], args[1]);
-		run.solve();
+		//debugging flags
+		if (args[0].substring(0, 2).equals("-o")) {
+
+			String debugCommand = args[0].substring(2);
+			
+			if ("options".equals(debugCommand)) {
+				System.out.println("Debugging options:");
+				System.out.println("-otray: prints debugging output for trays");
+				System.out.println("-osolver: prints debugging output for the solver");
+				System.out.println("-oposmoves: prints matrix representations of possible moves");
+				System.out.println("-ovalidmove: if call to validMove returns false, prints reason");	
+				System.out.println("-ostack: monitors size of tray stack - prints size on each iteration");		
+				System.exit(0);
+			}
+
+			Solver run = new Solver(args[1], args[2]);
+
+			else if ("tray".equals(debugCommand)) {
+				run.getTray().debug = true;
+			}
+
+			else if ("posmoves".equals(debugCommand)) {
+				run.getTray().debugPosMoves = true;
+			}
+
+			else if ("validmove".equals(debugCommand)) {
+				run.getTray().debugValidMove = true;
+			}
+
+			else if ("solver".equals(debugCommand)) {
+				run.debug = true;
+			}
+
+			else if ("stack".equals(debugCommand)) {
+				run.debugStack = true;
+			}
+
+			run.solve();
+
+		}
+
+		else {
+
+			Solver run = new Solver(args[0], args[1]);
+			run.solve();
+
+		}
 
 	}
 }
