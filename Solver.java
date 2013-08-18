01a79a7ff4c585e9cf958dff8004fb410b4efd7b
winstonjhuang <winstonjhuang@gmail.com>
2013-08-17 18:25:32 -0700

    Update Solver.java

--- Solver.java
+++ Solver.java
@@ -3,15 +3,11 @@ import java.util.*;
 public class Solver {
 
 	private InputSource input;
-	private Tray tray;
+	private Tray initTray;
 	public boolean debug;
 
-	public Solver () {
-		// not implemented yet
-	}
 
 	public Solver (String fileName) {
-		// Based on stuff from InputSource class -- is there an easier way to do this?
 		input = new InputSource(fileName);
 		String s = input.readLine ( );
 		makeTray(s);   // first line sets up the tray dimensions
@@ -25,7 +21,6 @@ public class Solver {
 	}
 
 	public Solver (String fileName, String goalName) {
-		// Based on stuff from InputSource class -- is there an easier way to do this?
 		input = new InputSource(fileName);
 		String s = input.readLine ( );
 		makeTray(s);   // first line sets up the tray dimensions
@@ -43,19 +38,19 @@ public class Solver {
 				break;
 			}
 			if (isBlock(s)) {
-				tray.addGoalBlock(new Block(s));
+				initTray.addGoalBlock(new Block(s));
 			}
 		}
 	}
 
 	public void makeTray (String s) {
 		String[] dimensions = s.split(" ");
-		tray = new Tray(Integer.valueOf(dimensions[0]), Integer.valueOf(dimensions[1]));
+		initTray = new Tray(Integer.valueOf(dimensions[0]), Integer.valueOf(dimensions[1]));
 	}
 
 	public void Parse (String line) {
 		if (isBlock(line)) {
-			tray.addBlock(new Block(line));
+			initTray.addBlock(new Block(line));
 		} else if (line.substring(0, 2).equals("-o")) {
 			String debugCommand = line.substring(2, line.length());
 			if ("options".equals(debugCommand)) {
@@ -65,12 +60,12 @@ public class Solver {
 				System.out.println("-osorter: " + "prints debugging output for the sorter");
 			}
 			if ("block".equals(debugCommand)) {
-				for (Block block : tray.getBlocks()) {
+				for (Block block : initTray.getBlocks()) {
 					block.debug = true;
 				}
 			}
 			if ("tray".equals(debugCommand)) {
-				tray.debug = true;
+				initTray.debug = true;
 			}
 			if ("solver".equals(debugCommand)) {
 				debug = true;
@@ -98,54 +93,52 @@ public class Solver {
 	}
 
 	public Tray getTray() {
-		return tray;
+		return initTray;
 	}
 
-	//The solve method assumes the tray argument passed in is NOT in the goal config.
-	public void solve (Tray t) {
-		HashSet<Tray> visited = new HashSet<Tray>();
-		visited.add(t);
-		/* 
-		The end of the LinkedList is the front of the Queue and beginning of Ll is back of queue
-		built-in linkedlist method removeLast() dequeus tray at front of queue and 
-		the built-in ll method addFirst(Tray) enqueues to the end of the trayQueue.
-		*/
-		LinkedList<Tray> trayQueue = new LinkedList<Tray>();
-		trayQueue.add(t);
-		while (!trayQueue.isEmpty()) {
-			Tray dQ;
-			dQ = trayQueue.removeFirst(); //dequeue
-			//iterate through all the possible moves for dQ
-			ArrayList<Tray> moves = dQ.posMoves();
+	public void solve () {
+
+		if (initTray.correctConfig()) {
+			System.exit(0);//puzzle is solved
+		}
+
+		HashSet<Tray> visited = new HashSet<Tray>();//set of trays that have been seen
+		LinkedList<Tray> process = new LinkedList<Tray>();//stack of trays to process
+
+		process.add(initTray);
+		visited.add(initTray);
+
+		while (!process.isEmpty()) {
+
+			Tray curr = process.removeFirst();
+			ArrayList<Tray> moves = curr.posMoves();
+
 			if (moves.isEmpty()) {
-				if (dQ.correctConfig()) {
-					System.out.println("No moves available.");
-					System.exit(0);
-
-
-				}
-
-
-			}
-			for  (int k = 0; k < dQ.posMoves().size(); k++) {
-				if (moves.get(k).correctConfig()) {
-					moves.get(k).printMoves(); // not a method yet. want to print out all moves to this point
-					System.out.println("SOLVED!!");
 					System.exit(1);
+			}
+
+			for  (Tray m : curr.posMoves()) {//process possible moves from current tray
+
+				if (m.correctConfig()) {//if the puzzle is solved:
+					m.printMoves(); 
+					System.exit(0);
 				}
-				if (!visited.contains(moves.get(k))) {
-					visited.add(moves.get(k)); // add the tray to the visited hashset.
-					trayQueue.addFirst(moves.get(k)); // enqueue
+
+				if (!visited.contains(m)) {//if this configuration has not been seen:
+					visited.add(m); // add the tray to the visited hashset.
+					process.addFirst(m); // add to stack
 				}
+
 			}
 		}
-		System.out.println("NO SOLUTION!");
-		System.exit(1); //this happens if there is no solution (as specified in piazza post 683).
+
+		System.exit(1); //this happens if there is no solution 
 	}
 
 	public static void main(String[] args) {
-		Solver start = new Solver(args[0], args[1]);
-		start.solve(start.getTray());
+
+		Solver run = new Solver(args[0], args[1]);
+		run.solve();
 
 	}
 }
